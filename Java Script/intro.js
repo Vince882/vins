099ef8b52819a475d53db14b3e166c47aa9f672f
het  ramani <hetramani186@gmail.com>
2024-01-19 08:41:47 +0530

    19-01-2024

--- Java Script/intro.js
+++ Java Script/intro.js
@@ -13,3 +13,18 @@ console.log(greeting); // Output: Goodbye, World!
 
 var messages = "Hello, World!";
 console.log(messages); // Output: Hello, World
+
+let xxy = 50;
+console.log(xxy); // output xxy 50
+ let message1 ="vince bhingradiya"
+ console.log(message1)
+
+ let yyx =40+20+44+18;
+
+ console.log(yyx); //output yyx 40+20+44+18
+
+var message2 ="java script"; 
+console.log(message2) //output message2 java script
+
+let xyx ="45-45";
+console.log(xyx) //output xyx 45-45 
