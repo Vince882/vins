2697b0a6ca6adbe3ee583669b8997295f187fd2c
het  ramani <hetramani186@gmail.com>
2024-01-19 08:20:44 +0530

    19-01-2024

---
+++ Java Script/intro.js
@@ -0,0 +1,15 @@
+let message = "Hello, World!";
+let x = 10;
+let y = 20;
+
+console.log(message); // Output: Hello, World!
+console.log(x + y); // Output: 30
+
+x = x + 5;
+console.log(x); // Output: 15
+
+let greeting = "Goodbye, World!";
+console.log(greeting); // Output: Goodbye, World!
+
+var messages = "Hello, World!";
+console.log(messages); // Output: Hello, World
