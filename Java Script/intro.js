5f1bf56cbeefff49b99974b7dbf9b22006b12431
het  ramani <hetramani186@gmail.com>
2024-01-23 08:17:53 +0530

    23-01-2024

--- Java Script/intro.js
+++ Java Script/intro.js
@@ -1,30 +1,54 @@
-let message = "Hello, World!";
-let x = 10;
-let y = 20;
+// let message = "Hello, World!";
+// let x = 10;
+// let y = 20;
 
-console.log(message); // Output: Hello, World!
-console.log(x + y); // Output: 30
+// console.log(message); // Output: Hello, World!
+// console.log(x + y); // Output: 30
 
-x = x + 5;
-console.log(x); // Output: 15
+// x = x + 5;
+// console.log(x); // Output: 15
 
-let greeting = "Goodbye, World!";
-console.log(greeting); // Output: Goodbye, World!
+// let greeting = "Goodbye, World!";
+// console.log(greeting); // Output: Goodbye, World!
 
-var messages = "Hello, World!";
-console.log(messages); // Output: Hello, World
+// var messages = "Hello, World!";
+// console.log(messages); // Output: Hello, World
 
-let xxy = 50;
-console.log(xxy); // output xxy 50
- let message1 ="vince bhingradiya"
- console.log(message1)
+// let xxy = 50;
+// console.log(xxy); // output xxy 50
+//  let message1 ="vince bhingradiya"
+//  console.log(message1)
 
- let yyx =40+20+44+18;
+//  let yyx =40+20+44+18;
 
- console.log(yyx); //output yyx 40+20+44+18
+//  console.log(yyx); //output yyx 40+20+44+18
 
-var message2 ="java script"; 
-console.log(message2) //output message2 java script
+// var message2 ="java script"; 
+// console.log(message2) //output message2 java script
 
-let xyx ="45-45";
-console.log(xyx) //output xyx 45-45 
+// let xyx ="45-45";
+// console.log(xyx) //output xyx 45-45 
+// let h1 ="hellow";
+// console.log(h1) //output h1 hellow
+
+
+let abc ="1+2*3";
+console.log(1+2*3)
+
+let abc1 =1+"2"*3;
+console.log(1+"2"*3)
+
+let abc2 ="1"+"2"*3;
+console.log("1"+"2"*3)
+
+
+
+
+
+
+let abc3 =1+2**3/2-1;
+console.log(1+2**3/2-1)
+
+let abc4 =9/2*3;
+console.log(9/2*3)
+
