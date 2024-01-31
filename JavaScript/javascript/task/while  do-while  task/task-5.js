18183004476acc01a07d7a3b70945c76946b6c84
Vince882 <SQ.vincebhingradiya@gmail.com>
2024-01-31 09:57:22 +0530

    31-01-2024

--- JavaScript/javascript/task/while  do-while  task/task-5.js
+++ JavaScript/javascript/task/while  do-while  task/task-5.js
@@ -1,10 +1,12 @@
-var fbnci = [0, 1];
+var fbnci = [0,1];
 var i = 2;
 
 do {
-   // Add the fibonacci sequence: add previous to one before previous
-   fbnci[i] = fbnci [i-2] + fbnci[i-1];
-   console.log(fbnci[i]);
-   fbnci[i]++;
-} 
+    /fbnci[i] = fbnci [i-2] + fbnci[i-1];
+    console.log(fbnci[i]);
+    fbnci[i]++;
+}
 while (fbnci[i] < 100);
+
+var fbnci = [0,1];
+var i = 2;
