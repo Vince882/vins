ff5dec734ba54b295b955d02dcaf1b293dc4cb71
Vince882 <SQ.vincebhingradiya@gmail.com>
2024-02-02 08:44:55 +0530

    02-02-2024

---
+++ JavaScript/6.RegeX-Math-Number/Math-Object.js
@@ -0,0 +1,53 @@
+// Math.E
+
+let math1 = Math.E
+console.log(math1);
+
+// Math.PI
+
+let math2 =Math.PI
+console.log(math2);
+
+// Math.round
+
+let math3 = Math.round(2.6)
+let math4 = Math.round(-2.6)
+console.log(math3);
+console.log(math4);
+
+// Math.ceil
+
+let  math5 = Math.ceil(10.9)
+let  math6 = Math.ceil(-10.9)
+console.log(math6);
+console.log(math5);
+
+// Math.floor
+
+let math7 = Math.floor(41.56)
+let math8 = Math.floor(-41.25)
+console.log(math8);
+console.log(math7);
+
+// Math.trunc
+
+let math9 = Math.trunc(-2.20)
+console.log(math9);
+
+// Math.sign
+
+let math10 = Math.sign()
+let math11 = Math.sign(0)
+console.log(math11);
+console.log(math10);
+
+// Math.pow
+
+let math12 = Math.pow(2 , 5)
+console.log(math12);
+
+var x = 0;
+if (10>9>8) {
+    x += 1; 
+}
+console.log(x);
---
+++ JavaScript/JavaScript task/task-1.js
@@ -0,0 +1,50 @@
+
+// task-1
+var x = 0;
+if (10 > 9 > 8) {
+    x += 1;
+}
+
+console.log(x);
+
+
+var x1 = 100;
+if (10 > 9 > 8) {
+    x += 1;
+}
+
+console.log(x1);
+
+// // task-2
+// var bar = function foo () {};
+// console.log(bar === foo);
+
+
+// task-3
+
+console.log(1+2+'1');
+console.log('1'+1+2);
+
+// task-4
+
+console.log("9" > "19");
+console.log("09" > "19");
+
+// task-5
+
+const f = new Boolean(false);
+if (f) {
+    console.log(1);
+} else {
+    console.log(2);
+}
+console.log(typeof f);
+
+// // task-6
+
+// let x = 100;
+// let y = x++;
+// let z = ++x;
+
+// let n = (x == y) ? z++ : ++z;
+
